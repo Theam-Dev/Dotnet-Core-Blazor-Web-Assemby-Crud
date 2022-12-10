a74ba416600836822f08d845c7f875be10fd5f0d
theam-dev <phearakroun@gmail.com>
2022-12-10 11:46:28 +0700

    Blazor Crud

---
+++ BlazorCrud/Server/Controllers/PostsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazorCrud.Server.Data;
+using BlazorCrud.Shared.Models;
+
+namespace BlazorCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public PostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Posts
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var dev = await _context.Posts.ToListAsync();
+            return Ok(dev);
+        }
+        // GET: api/Posts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+        // PUT: api/Posts/5
+        [HttpPut]
+        public async Task<IActionResult> Put(Post post)
+        {
+            _context.Entry(post).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        // POST: api/Posts
+        [HttpPost]
+        public async Task<IActionResult> Post(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+            return Ok(post.Id);
+        }
+        // DELETE: api/Posts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var dev = new Post { Id = id };
+            _context.Remove(dev);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ BlazorCrud/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCrud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace BlazorCrud.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Post>  Posts { get; set; }
+    }
+}
